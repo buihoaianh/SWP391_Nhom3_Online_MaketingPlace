588520196e0229c20a3aa7261ba9003e5036dd3c
TuNguyen0110 <100578720+TuNguyen011@users.noreply.github.com>
2025-06-17 17:20:46 +0700

    Add Feature Product Detail, Home

---
+++ MarketingPlace/src/java/model/Product.java
@@ -0,0 +1,124 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author tulok
+ */
+public class Product {
+    private int productId;
+    private int accountId;
+    private String imageURL;
+    private String productName;
+    private String price;
+    private int quantity;
+    private int categoryID;
+    private int discountId;
+    private Date createProductDate;
+    private String description;
+
+    public Product(int productId, int accountId, String imageURL, String productName, String price, int quantity, int categoryID, int discountId, Date createProductDate, String description) {
+        this.productId = productId;
+        this.accountId = accountId;
+        this.imageURL = imageURL;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+        this.categoryID = categoryID;
+        this.discountId = discountId;
+        this.createProductDate = createProductDate;
+        this.description = description;
+    }
+
+    
+    
+
+    public Product() {
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public Date getCreateProductDate() {
+        return createProductDate;
+    }
+
+    public void setCreateProductDate(Date createProductDate) {
+        this.createProductDate = createProductDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDiscountId() {
+        return discountId;
+    }
+
+    public void setDiscountId(int discountId) {
+        this.discountId = discountId;
+    }
+    
+}
